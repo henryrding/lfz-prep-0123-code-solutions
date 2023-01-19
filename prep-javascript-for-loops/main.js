a9cb9dd57ee4dadf1d60fbfc3cceacd1129c878c
Henry Ding <henryrding@yahoo.com>
2023-01-19 02:59:26 +0000

    Completed prep-javascript-for-loops exercise.

---
+++ prep-javascript-for-loops/main.js
@@ -0,0 +1,13 @@
+for (let i = 0; i < 10; i++) {
+  console.log(i);
+}
+
+let value = 0;
+for (let j = 0; j < 10; j++) {
+  console.log(value);
+  value += 2;
+}
+
+for (let k = 100; k >= 0; k--) {
+  console.log('Time till explosion:', k + '!');
+}
